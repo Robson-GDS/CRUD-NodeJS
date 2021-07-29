7e95dec11bd03f89d47d6b5298f97577ef736313
Robson Gomes <robson.080490@gmail.com>
2021-07-29 11:39:59 -0300

    add  rota de listagem

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const server = express();
 
 const cursos = ['Node JS', 'Javascript', 'React Native']
 
-server.get('/curso/:index', (request, response) => {
+server.get('/cursos', (request, response) => {
+  return response.json(cursos);
+});
+
+server.get('/cursos/:index', (request, response) => {
   const { index } = request.params;
 
 
